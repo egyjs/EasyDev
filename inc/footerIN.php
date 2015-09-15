122d684c1abca562ca69aca58e9f3d1d1bd4561b
Ebram Marzouk <abram.atef@gmail.com>
2015-09-15 04:25:04 +0300

    Fix XSS

--- inc/footerIN.php
+++ inc/footerIN.php
@@ -1,6 +1,6 @@
     <div id="footer">
         <nav class="float-left flex-center-left">
-            &copy; 2015-2016<a href="<?php echo $_SERVER['PHP_SELF']; ?>">HOME</a>IDE Inc.
+            &copy; 2015-2016<a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">HOME</a>IDE Inc.
         </nav>
         <nav class="float-right flex-center-right">
             d
